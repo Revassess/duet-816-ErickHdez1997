8ea4d9a79fa9e9b89bd9f1fa2c58f17a19808b55
ErickHdez1997 <Erick.Hdez199721@gmail.com>
2020-08-10 15:32:43 -0400

    first three problems

---
+++ Revassess/src/sql/answer2.sql
@@ -0,0 +1,4 @@
+insert into app_user
+	(username, password, first_name, last_name, role_id)
+values
+	('Erick_Hernandez', 'Erick123', 'Erick', 'Hernandez', '4');
---
+++ Revassess/src/sql/answer3.sql
@@ -0,0 +1,3 @@
+select *
+from app_user, study_set
+where role_id = 5;
